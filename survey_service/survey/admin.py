f8cd7c6d4d9a07c81991f03c7c8aa12dc5ab3c1b
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-11-28 23:41:35 +0300

    SurveyAdmin

--- survey_service/survey/admin.py
+++ survey_service/survey/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Survey, SurveyQuestion
+
+
+class SurveyQuestionInline(admin.TabularInline):
+    model = SurveyQuestion
+    extra = 0
+
+
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_from', 'date_to')
+    list_filter = ('name', 'date_from', 'date_to')
+    search_fields = ('name', 'description')
+    fields = ('name', 'description', 'date_to')
+
+    inlines = [
+        SurveyQuestionInline,
+    ]

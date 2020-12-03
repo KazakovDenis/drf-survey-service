e537dbc6ec99ecde67713d36648a7338002d3c81
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-12-03 20:06:00 +0300

    STATIC_ROOT

--- survey_service/config/settings/base.py
+++ survey_service/config/settings/base.py
@@ -130,9 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = ROOT / 'static'
 
 STATICFILES_DIRS = [
-    ROOT / 'static',
     API_V1_DIR / 'static',
 ]
 

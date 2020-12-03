92c8eeef2a983d7358df4d1455b546afa182e933
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-12-03 14:47:15 +0300

    token authentication

--- survey_service/api/urls.py
+++ survey_service/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from rest_framework.authtoken import views
 
 from .views import api_versions_list
 
@@ -6,5 +7,6 @@ from .views import api_versions_list
 urlpatterns = [
     path('', api_versions_list, name='api-versions-list'),
     path('auth/', include('rest_framework.urls')),
+    path('auth/get-token/', views.obtain_auth_token),
     path('v1/', include('api.v1.urls')),
 ]

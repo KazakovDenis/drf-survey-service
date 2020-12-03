6565a2456c513f4eb42307d03b45b6075e3d42bf
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-12-03 15:30:02 +0300

    DEFAULT_PERMISSION_CLASSES

--- survey_service/config/settings/base.py
+++ survey_service/config/settings/base.py
@@ -144,8 +144,4 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    # read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
 }

f2d48a9e60f35ea52259e045e39a486509bcb914
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-12-05 14:57:41 +0300

    test_delete_scheme

--- survey_service/api/v1/tests/test_schemes.py
+++ survey_service/api/v1/tests/test_schemes.py
@@ -5,11 +5,13 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
+from survey.models import Scheme
 from .common import *
 
 
 class SchemeTest(APITestCase):
 
+    scheme = None
     content_type = 'application/json'
     credentials = {
         'username': 'test_user',
@@ -22,14 +24,28 @@ class SchemeTest(APITestCase):
         super().setUpClass()
         user_model = get_user_model()
         user_model.objects.create_superuser(**cls.credentials)
+        cls.scheme = Scheme.objects.create(name=random_str())
 
     def setUp(self):
         self.client.login(**self.credentials)
 
-    def test_get_schemes(self):
+    def test_get_scheme_list(self):
         url = reverse('scheme-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        schemes = response.data.get('results', [])
+        scheme_id = str(self.scheme.id)
+        scheme_ids = list(
+            filter(lambda item: item['id'] == scheme_id, schemes)
+        )
+        self.assertEqual(len(scheme_ids), 1)
+
+    def test_get_scheme(self):
+        url = reverse('scheme-detail', kwargs={'pk': self.scheme.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('name'), self.scheme.name)
 
     def test_create_scheme(self):
         url = reverse('scheme-list')
@@ -40,7 +56,7 @@ class SchemeTest(APITestCase):
             Case('Minimal', 201, {'name': non_unique}),
             # todo: Case('Name exists', {'name': non_unique, 'date_from': TOMORROW, 'date_to': TOMORROW}, 201),
             Case(
-                'Full', 201,
+                'Full survey info', 201,
                 {'name': random_str(), 'date_from': TODAY, 'date_to': TOMORROW, 'description': random_str()},
             ),
             Case(
@@ -67,3 +83,14 @@ class SchemeTest(APITestCase):
                     url, data=dumps(case.data), content_type=self.content_type
                 )
                 self.assertEqual(response.status_code, case.code)
+
+    # todo
+    def t_edit_scheme(self):
+        # todo: add, edit, delete questions
+        pass
+
+    def test_delete_scheme(self):
+        scheme = Scheme.objects.create(name=random_str())
+        url = reverse('scheme-detail', kwargs={'pk': scheme.id})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
